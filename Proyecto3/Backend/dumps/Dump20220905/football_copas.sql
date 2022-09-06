9680c39267e1ad328b8b3046be605b1bc4ad8499
davalbra <davalbra@espol.edu.ec>
2022-09-05 23:00:09 -0500

    Agregando database

---
+++ Proyecto3/Backend/dumps/Dump20220905/football_copas.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: football
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `copas`
+--
+
+DROP TABLE IF EXISTS `copas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `copas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) DEFAULT NULL,
+  `cantidad` int DEFAULT NULL,
+  `views` int DEFAULT NULL,
+  `members` int DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `organizacionid` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `copas_organizacion_association` (`organizacionid`),
+  CONSTRAINT `copas_organizacion_association` FOREIGN KEY (`organizacionid`) REFERENCES `organizacions` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `copas`
+--
+
+LOCK TABLES `copas` WRITE;
+/*!40000 ALTER TABLE `copas` DISABLE KEYS */;
+INSERT INTO `copas` VALUES (1,'copa 0',10,154449656,36374,'2022-09-05 03:57:36','2022-09-05 03:57:36',3),(2,'copa 1',11,468555164,78773,'2022-09-05 03:57:36','2022-09-05 03:57:36',1),(3,'copa 2',12,540152599,53595,'2022-09-05 03:57:36','2022-09-05 03:57:36',2),(4,'copa 3',13,662563842,87711,'2022-09-05 03:57:36','2022-09-05 03:57:36',2),(5,'copa 4',14,886610916,98879,'2022-09-05 03:57:36','2022-09-05 03:57:36',1),(6,'copa 5',15,168091764,90040,'2022-09-05 03:57:36','2022-09-05 03:57:36',1),(7,'copa 6',16,494289468,25914,'2022-09-05 03:57:36','2022-09-05 03:57:36',4),(8,'copa 7',17,887450026,63844,'2022-09-05 03:57:36','2022-09-05 03:57:36',3),(9,'copa 8',18,360923571,88680,'2022-09-05 03:57:36','2022-09-05 03:57:36',4),(10,'copa 9',19,417657175,41443,'2022-09-05 03:57:36','2022-09-05 03:57:36',4);
+/*!40000 ALTER TABLE `copas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-05 22:59:05
